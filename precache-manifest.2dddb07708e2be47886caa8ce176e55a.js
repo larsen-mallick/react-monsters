ca86698c5b9b1690fb945fd48f000ba1b5041447
larsen-mallick <larsen.mallick@gmail.com>
2020-09-02 17:54:27 +0530

    Updates

---
+++ precache-manifest.2dddb07708e2be47886caa8ce176e55a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0351f5444b0956aeb900ff513e6a337",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "73de8cb22d16a3480c97",
+    "url": "/react-monsters/static/css/main.6b32008b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73de8cb22d16a3480c97",
+    "url": "/react-monsters/static/js/main.811b407d.chunk.js"
+  },
+  {
+    "revision": "90fbba813a35afb5d768",
+    "url": "/react-monsters/static/js/runtime-main.1ac8d056.js"
+  }
+]);
